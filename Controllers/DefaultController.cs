09f69d94550873263a27138a0dd9d1dcbf9a15db
kiet <mkla1252006@gmail.com>
2025-10-20 16:57:35 +0700

    Add project files.

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn_web.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Cart()
+        {
+            return View();
+        }
+        public ActionResult UserLogin()
+        {
+            return View();
+        }
+        public ActionResult TrangIphone()
+        {
+            return View();
+        }
+        public ActionResult TrangMac()
+        {
+            return View();
+        }
+        public ActionResult TrangWatch()
+        {
+            return View();
+        }
+        public ActionResult TrangIpad()
+        {
+            return View();
+        }
+        public ActionResult TrangMussic()
+        {
+            return View();
+        }
+        public ActionResult TrangCamera()
+        {
+            return View();
+        }
+        public ActionResult TrangAccessory()
+        {
+            return View();
+        }
+    }
+}
